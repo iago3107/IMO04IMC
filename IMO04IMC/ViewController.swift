d0d662a6942aa5491ecf77ee8a4c392112b6e38b
Iago <iago.jose@icloud.com>
2020-10-15 14:21:41 -0300

    ta rolando pae

--- IMO04IMC/ViewController.swift
+++ IMO04IMC/ViewController.swift
@@ -9,13 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tfweight: UITextField!
+    @IBOutlet weak var tfheight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    @IBOutlet weak var viResult: UIView!
+    
+    var imc:Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
     }
 
-
+    @IBAction func calculate(_ sender: Any) {
+        if let weight = Double(tfweight.text!), let height = Double(tfheight.text!){
+            imc = weight / (height*height)
+            showResults()
+        }
+    }
+    
+    func showResults()  {
+        var result: String = ""
+        var imagem: String = ""
+        switch imc {
+            case 0..<16:
+                result = "Magreza"
+                imagem = "abaixo"
+            case 16..<18.5:
+                result = "abaixo do peso"
+                imagem = "abaixo"
+            case 18.5..<25:
+                result = "Peso ideal"
+                imagem = "ideal"
+            case 25..<30:
+                result = "Sobre Peso"
+                imagem = "sobre"
+            default:
+                result = "Obesidade"
+                imagem = "obesidade"
+        }
+        lbResult.text = result
+        ivResult.image = UIImage(named: imagem)
+        viResult.isHidden = false
+    }
 }
 
